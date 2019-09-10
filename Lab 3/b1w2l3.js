203f30618f507f8e8ebcb12afcf1c69a375f41fd
Mike Karssen <99038898@mydavinci.nl>
2019-09-10 14:12:04 +0200

    Lab 3

---
+++ Lab 3/b1w2l3.js
@@ -0,0 +1,29 @@
+//© Michael Karssen 99038898
+//Applicatie en media ontwikkelaar
+
+
+document.write("1 x 6 = " + 1*6 + "<br/>");
+document.write("2 x 6 = " + 2*6 + "<br/>");	
+document.write("3 x 6 = " + 3*6 + "<br/>");	
+document.write("4 x 6 = " + 4*6 + "<br/>");	
+document.write("5 x 6 = " + 5*6 + "<br/>");	
+document.write("6 x 6 = " + 6*6 + "<br/>");	
+document.write("7 x 6 = " + 7*6 + "<br/>");	
+document.write("8 x 6 = " + 8*6 + "<br/>");	
+document.write("9 x 6 = " + 9*6 + "<br/>");	
+document.write("10 x 6 = " + 10*6 + "<br/>");	
+
+document.write("1 x 3 = " + 1*3 + "<br/>");
+document.write("2 x 3 = " + 2*3 + "<br/>");	
+document.write("3 x 3 = " + 3*3 + "<br/>");	
+document.write("4 x 3 = " + 4*3 + "<br/>");	
+document.write("5 x 3 = " + 5*3 + "<br/>");	
+document.write("6 x 3 = " + 6*3 + "<br/>");	
+document.write("7 x 3 = " + 7*3 + "<br/>");	
+document.write("8 x 3 = " + 8*3 + "<br/>");	
+document.write("9 x 3 = " + 9*3 + "<br/>");	
+document.write("10 x 3 = " + 10*3 + "<br/>");	
+
+
+
+
